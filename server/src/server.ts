c10a7ea5070291760e53c302d002dc6134167ae8
Abu Bakr Siddik <limonsk026@gmail.com>
2025-07-02 14:57:18 +0600

    server deploy cors policy setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,12 +4,13 @@ import mongoose from "mongoose";
 import config from "./config";
 
 let server: Server;
+const PORT = parseInt(config.port || "3000",10);
 
 async function main() {
   try {
     await mongoose.connect(config.db_uri!);
     console.log("✅ Database connected");
-    server = app.listen(config.port, () => {
+    server = app.listen(PORT,"0.0.0.0", () => {
       console.log(`✅ Server running on port ${config.port}`);
     });
   } catch (error) {
